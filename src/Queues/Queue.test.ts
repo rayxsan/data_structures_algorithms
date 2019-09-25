543be43120db0297bbb9002d93c87f2a509147f2
Raymel Manreza <raymel.manreza@gmail.com>
2019-09-24 21:26:57 -0400

    Queue methods work in progress

--- src/Queues/Queue.test.ts
+++ src/Queues/Queue.test.ts
@@ -1,9 +1,9 @@
 import Queue from "../Queues/Queue";
 
 describe("Queue", () => {
-  const a = [1, 2, 3, 4, 5];
-  const myQueue = new Queue<number>();
   test("Is Empty? ", () => {
+    const a = [1, 2, 3, 4, 5];
+    const myQueue = new Queue<number>();
     expect(myQueue.isEmpty()).toBeTruthy;
     for (let i = 0; i < a.length; i++) {
       myQueue.enqueue(a[i]);
@@ -11,6 +11,8 @@ describe("Queue", () => {
     expect(myQueue.isEmpty()).toBeFalsy;
   });
   test("Remove element", () => {
+    const a = [1, 2, 3, 4, 5];
+    const myQueue = new Queue<number>();
     const b: number[] = [];
     for (let i = 0; i < a.length; i++) {
       myQueue.enqueue(a[i]);
@@ -22,5 +24,15 @@ describe("Queue", () => {
     for (let i = 0; i < a.length; i++) {
       expect(a[i]).toBe(b[i]);
     }
+  });
+
+  test("Size of the queue", () => {
+    const a = [1, 2, 3, 4, 5];
+    const myQueue = new Queue<number>();
+    expect(myQueue.size()).toBe(0);
+    for (let i = 0; i < a.length; i++) {
+      myQueue.enqueue(a[i]);
+    }
+    expect(myQueue.size()).toBe(a.length);
   });
 });
--- src/Queues/Queue.ts
+++ src/Queues/Queue.ts
@@ -1,14 +1,25 @@
+import List from "../Lists/List";
+
 class Queue<T> {
-  constructor() {}
-  enqueue(item: T) {}
+  list: List<T>;
+  constructor() {
+    this.list = new List<T>();
+  }
+  enqueue(item: T) {
+    this.list.insertLast(item);
+  }
   dequeue(): T {
     return {} as T;
   }
   isEmpty(): boolean {
-    return true;
+    return this.list.isEmpty();
   }
   size(): number {
-    return 0;
+    let size = 0;
+    this.list.forEach((elem: T) => {
+      size = size + 1;
+    });
+    return size;
   }
 }
 
