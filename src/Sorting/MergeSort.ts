3831280f385548a34bea77ed98295b5db7723ee9
Raymel Manreza <raymel.manreza@gmail.com>
2019-09-25 21:41:22 -0400

    Sorts

---
+++ src/Sorting/MergeSort.ts
@@ -0,0 +1,9 @@
+/**
+ * array: array of elements to be sorted
+ * cmp: return -1 if a < b; 0 if a === b; and 1 if b > a
+ */
+function mergeSort<T>(array: T[], cmp: (a: T, b: T) => number): number[] {
+  return [];
+}
+
+export default mergeSort;
---
+++ src/Sorting/QuickSort.ts
@@ -0,0 +1,7 @@
+/**
+ * array: array of elements to be sorted
+ * cmp: return -1 if a < b; 0 if a === b; and 1 if b > a
+ */
+function quickSort<T>(array: T[], cmp: (a: T, b: T) => number): T[] {
+  return [];
+}
