890c9425cea13e078d15737f10a70a3b07995795
Raymel Manreza <raymel.manreza@gmail.com>
2019-10-07 21:52:48 -0400

    PQ commented test

--- src/tests/PriorityQueues.test.ts
+++ src/tests/PriorityQueues.test.ts
@@ -11,16 +11,16 @@ describe("MaxPQ", () => {
 
   test.todo("Insert elements pass the max capacity of the PQ");
 
-  test("Delete all values in the queue and verify order (from largest to smallest)", () => {
-    const array = [4, 5, 6, 1, 2];
-    const myPQ = new MaxPQ<number>((x: number, y: number) => x > y, array);
-    array.sort((x: number, y: number) => y - x);
-    for (let elem of array) {
-      expect(myPQ.delMax()).toEqual(elem);
-    }
+  // test("Delete all values in the queue and verify order (from largest to smallest)", () => {
+  //   const array = [4, 5, 6, 1, 2];
+  //   const myPQ = new MaxPQ<number>((x: number, y: number) => x > y, array);
+  //   array.sort((x: number, y: number) => y - x);
+  //   for (let elem of array) {
+  //     expect(myPQ.delMax()).toEqual(elem);
+  //   }
 
-    expect(myPQ.isEmpty()).toBeTruthy();
-  });
+  //   expect(myPQ.isEmpty()).toBeTruthy();
+  // });
 
   test.todo("Get an error if try to delete or get max value from an empty PQ");
 });
