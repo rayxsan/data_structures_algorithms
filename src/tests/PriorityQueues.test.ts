495b93baf57b486d897bfe8f3888330013853aff
Leonardo Marmol <lmarmol@eitrsystems.com>
2019-10-20 12:14:30 -0700

    Test: fill and empty PQ multiple times

--- src/tests/PriorityQueues.test.ts
+++ src/tests/PriorityQueues.test.ts
@@ -2,6 +2,7 @@ import { MaxPQ } from "../Queues/PriorityQueues";
 
 describe("MaxPQ", () => {
   let cmpAscending = (x: number, y: number) => x > y;
+  let cmpDescending = (x: number, y: number) => x < y;
   test("Create an empty PQ", () => {
     let myPQ = new MaxPQ(cmpAscending);
     expect(myPQ.isEmpty()).toBeTruthy();
@@ -35,5 +36,27 @@ describe("MaxPQ", () => {
     const myPQ = new MaxPQ<number>(cmpAscending);
     expect(() => myPQ.delMax()).toThrowError("Is empty");
     expect(() => myPQ.max()).toThrowError("Is empty");
+  });
+
+  test("Fill up and empty out the queue multiple times", () => {
+    const myPQ = new MaxPQ<number>(cmpDescending);
+
+    const numLoops = 2;
+    const opsPerLoop = 10;
+
+    for (let loop = 0; loop < numLoops; loop++) {
+      // filling up loop
+      for (let i = 0; i < opsPerLoop; i++) {
+        const val = loop * opsPerLoop + i;
+        myPQ.insert(val);
+      }
+
+      // emptying out loop
+      for (let i = 0; i < opsPerLoop; i++) {
+        const expectedVal = loop * opsPerLoop + i;
+        const currentValue = myPQ.delMax();
+        expect(currentValue).toBe(expectedVal);
+      }
+    }
   });
 });
