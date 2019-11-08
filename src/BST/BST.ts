29c9db9341a2a7d225773e02663d76272e649a8e
Raymel Manreza <raymel.manreza@gmail.com>
2019-11-07 22:26:39 -0500

    BST work in progress

--- src/BST/BST.ts
+++ src/BST/BST.ts
@@ -1,5 +1,7 @@
 import { is } from "@babel/types";
 
+type NodeLabel = "root" | "left" | "right";
+
 class BSTNode<K, V> {
   key: K;
   value: V;
@@ -38,8 +40,7 @@ export class BST<K, V> {
    */
 
   private sizeNode(node: BSTNode<K, V> | null): number {
-    if (node) return node.nodesCount;
-    else return 0;
+    return node ? node.nodesCount : 0;
   }
 
   size() {
@@ -48,14 +49,14 @@ export class BST<K, V> {
     } else throw new Error("Is empty");
   }
 
-  private getRecursive(node: BSTNode<K, V> | null, key: K): V | null {
+  private getRecursive(node: BSTNode<K, V> | null, key: K): V {
     if (node) {
-      if (this.cmp(key, node.key) <= 0) {
+      const cmpResult = this.cmp(key, node.key);
+      if (cmpResult < 0) {
         return this.getRecursive(node.left, key);
-      } else if (this.cmp(key, node.key) > 0) {
-        if (node.right) return this.getRecursive(node.right, key);
-      }
-      return node.value;
+      } else if (cmpResult > 0) {
+        return this.getRecursive(node.right, key);
+      } else return node.value;
     } else throw new Error("Not found");
   }
 
@@ -69,9 +70,10 @@ export class BST<K, V> {
     value: V
   ): BSTNode<K, V> {
     if (node) {
-      if (this.cmp(key, node.key) <= 0) {
+      const cmpResult = this.cmp(key, node.key);
+      if (cmpResult < 0) {
         node.left = this.putRecursive(node.left, key, value);
-      } else if (this.cmp(key, node.key) > 0) {
+      } else if (cmpResult > 0) {
         node.right = this.putRecursive(node.right, key, value);
       } else node.value = value;
       node.nodesCount =
@@ -85,22 +87,88 @@ export class BST<K, V> {
     this.root = this.putRecursive(this.root, key, value);
   }
 
-  has(key: K) {
-    return true;
+  private hasRecursive(node: BSTNode<K, V> | null, key: K): boolean {
+    if (node) {
+      const cmpResult = this.cmp(key, node.key);
+      if (cmpResult < 0) {
+        return this.hasRecursive(node.left, key);
+      } else if (cmpResult > 0) {
+        return this.hasRecursive(node.right, key);
+      } else return true;
+    } else return false;
   }
-
+  has(key: K) {
+    return this.hasRecursive(this.root, key);
+  }
+  private minRecursive(node: BSTNode<K, V> | null): BSTNode<K, V> {
+    if (node) {
+      if (node.left) {
+        return this.minRecursive(node.left);
+      }
+      return node;
+    } else throw new Error("Not found");
+  }
+  min(): K {
+    return this.minRecursive(this.root).key;
+  }
+  private deleteMinRecursive(node: BSTNode<K, V> | null): BSTNode<K, V> | null {
+    if (node) {
+      if (node.left) {
+        node.left = this.deleteMinRecursive(node.left);
+        node.nodesCount =
+          this.sizeNode(node.left) + this.sizeNode(node.right) + 1;
+        return node;
+      }
+      return node.right;
+    } else throw new Error("Not found");
+  }
+  deleteMin() {
+    this.root = this.deleteMinRecursive(this.root);
+  }
   /**
    * *Delete entry with key @key
    * If no such entry is found, throw error
    */
 
-  private delRecursive(node: BSTNode<K, V>, key: K): V {
-    return {} as V;
-  }
-  del(key: K): V {
-    if (this.root) {
-      return this.delRecursive(this.root, key);
+  private delRecursive(node: BSTNode<K, V> | null, key: K): BSTNode<K, V> {
+    if (node) {
+      const cmpResult = this.cmp(key, node.key);
+      if (cmpResult < 0) {
+        node.left = this.delRecursive(node.left, key);
+      } else if (cmpResult > 0) {
+        node.right = this.delRecursive(node.right, key);
+      } else {
+        if (node.right) {
+          if (node.left) {
+            let t = node;
+            node = this.minRecursive(t.right);
+            node.right = this.deleteMinRecursive(t.right);
+            node.left = t.left;
+          }
+          if (node.right) return node.right;
+        }
+        if (node.left) return node.left;
+      }
+      node.nodesCount =
+        this.sizeNode(node.left) + this.sizeNode(node.right) + 1;
+      return node;
     } else throw new Error("Not found");
+  }
+  del(key: K) {
+    this.root = this.delRecursive(this.root, key);
+  }
+
+  printRecursive(node: BSTNode<K, V>, label: NodeLabel, indent = 0) {
+    const spaces = "  ".repeat(indent);
+    console.log(
+      `${spaces}Node(${label}): [key: ${node.key}, value: ${node.value}]`
+    );
+    if (node.left) this.printRecursive(node.left, "left", indent + 1);
+    if (node.right) this.printRecursive(node.right, "right", indent + 1);
+  }
+
+  print() {
+    if (this.root) this.printRecursive(this.root, "root");
   }
 
   /**
@@ -117,15 +185,58 @@ export class BST<K, V> {
   ) {}
 }
 
-const elements = [
+let myBST = new BST<number, string>((x: number, y: number) => x - y);
+interface Pair<K, V> {
+  key: K;
+  value: V;
+}
+
+const elements: Pair<number, string>[] = [
   { key: 1, value: "a" },
   { key: 2, value: "b" },
   { key: 3, value: "c" },
   { key: 4, value: "d" },
-  { key: 5, value: "e" }
+  { key: 5, value: "e" },
+  { key: 6, value: "f" },
+  { key: 7, value: "g" }
 ];
-let myBST = new BST<number, string>((x: number, y: number) => x - y);
-myBST.put(1, "a");
-myBST.put(2, "b");
 
-console.log(myBST.get(1));
+function insertElementsInTreeRecursive(
+  tree: BST<number, string>,
+  elements: Pair<number, string>[],
+  lo: number,
+  hi: number
+) {
+  if (lo < hi) {
+    const mid = Math.floor(lo + (hi - lo) / 2);
+
+    const elem = elements[mid];
+    tree.put(elem.key, elem.value);
+
+    insertElementsInTreeRecursive(tree, elements, lo, mid);
+    insertElementsInTreeRecursive(tree, elements, mid + 1, hi);
+  }
+}
+
+function insertElementsInTree(
+  tree: BST<number, string>,
+  elements: Pair<number, string>[]
+) {
+  elements.sort((a, b) => a.key - b.key);
+  // [a, b)
+  insertElementsInTreeRecursive(tree, elements, 0, elements.length);
+}
+
+insertElementsInTree(myBST, elements);
+
+/**
+ *        4
+ *      /   \
+ *     2     6
+ *    / \   /  \
+ *   1   3  5   7
+ */
+
+//myBST.print();
+myBST.deleteMin();
+console.log(myBST.min());
