0db724657fe4ebc82ade72f5c1fab823e13c1392
Raymel Manreza <raymel.manreza@gmail.com>
2020-01-25 00:15:49 -0500

    Set work in progress

--- src/Set/Set.ts
+++ src/Set/Set.ts
@@ -1,10 +1,82 @@
+import List from "../Lists/List";
+import hash from "../utils/Hashing";
+import { runInThisContext } from "vm";
+
+const defaultInitialCapacity = 4;
+
+interface ValueWrapper<T> {
+  hash: number;
+  value: T;
+}
+
 class Set<T> {
+  private set: Array<List<ValueWrapper<T>>>;
+  private setList: List<ValueWrapper<T>>;
+  private size: number;
+  private capacity: number;
+
+  constructor(initialCapacity: number = defaultInitialCapacity) {
+    this.capacity = Math.max(defaultInitialCapacity, initialCapacity);
+    this.size = 0;
+    this.set = [];
+    this.setList = new List();
+  }
+
+  private compare(x: T, y: T): boolean {
+    return x === y;
+  }
+
+  private growSet() {
+    const newCapacity = this.capacity * 2;
+    const tempArr = new Array<List<ValueWrapper<T>>>();
+
+    for (let i = 0; i < this.capacity; i++) {
+      const list = this.set[i];
+      if (List) {
+        list.forEach(wrapper => {
+          const newIdx = wrapper.hash % newCapacity;
+          tempArr[newIdx] = new List();
+          tempArr[newIdx].insertLast(wrapper);
+        });
+      }
+    }
+
+    this.capacity = newCapacity;
+    this.set = tempArr;
+  }
+
+  getCapacity() {
+    return this.capacity;
+  }
+
   /**
    * The add() method appends a new element with a specified value to the end of a Set object.
    * @param value The value of the element to add to the Set object.
    */
   add(value: T): Set<T> {
-      return this;
+    const loadFactor = this.size / this.capacity;
+    const hashCode = hash(value);
+    const wrapper: ValueWrapper<T> = {
+      hash: hashCode,
+      value: value
+    };
+
+    if (loadFactor > 1) {
+      this.growSet();
+    }
+
+    const idx = hashCode % this.capacity;
+    this.set[idx] = this.setList;
+
+    for (let elements of this.set[idx]) {
+      if (this.compare(wrapper.value, elements.value)) {
+        throw new Error("Duplicate entry");
+      }
+    }
+    this.set[idx].insertLast(wrapper);
+    this.size++;
+
+    return this;
   }
 
   /**
@@ -18,6 +90,16 @@ class Set<T> {
    * @param value The value to test for presence in the Set object.
    */
   has(value: T): boolean {
+    const hashCode = hash(value);
+    const idx = hashCode % this.capacity;
+    this.set[idx] = new List();
+    if (List) {
+      for (let wrapper of this.set[idx]) {
+        if (wrapper.value === value) {
+          return true;
+        }
+      }
+    }
     return false;
   }
 
@@ -30,7 +112,7 @@ class Set<T> {
    * returns the number of (unique) elements in a Set
    */
   getSize(): number {
-    return 0;
+    return this.size;
   }
 
   /**
--- src/tests/Set.test.ts
+++ src/tests/Set.test.ts
@@ -1,11 +1,41 @@
 import Set from "../Set/Set";
 
-describe("HashTable", () => {
-  test.todo("add");
+describe("Sets", () => {
+  test("add", () => {
+    const mySet = new Set();
+    for (let i = 1; i <= 4; i++) {
+      mySet.add(i);
+      expect(mySet.getSize()).toEqual(i);
+    }
+    expect(mySet.getCapacity()).toEqual(4);
+    mySet.add(5).add(6);
+    expect(mySet.getCapacity()).toEqual(8);
+
+    expect(() => mySet.add(2)).toThrowError("Duplicate entry");
+  });
+
   test.todo("delete");
-  test.todo("has");
+
+  test("has", () => {
+    const mySet = new Set();
+    for (let i = 0; i < 6; i++) {
+      mySet.add(i);
+      expect(mySet.has(i)).toBeTruthy;
+    }
+    expect(mySet.has(34)).toBeFalsy; // this should fail
+  });
+
   test.todo("clear");
-  test.todo("getSize");
+
+  test("getSize", () => {
+    const mySet = new Set();
+    for (let i = 0; i < 14; i++) {
+      mySet.add(i);
+      expect(mySet.getSize()).toEqual(i + 1);
+    }
+  });
+
   test.todo("forEach");
+
   test.todo("iterator");
 });
